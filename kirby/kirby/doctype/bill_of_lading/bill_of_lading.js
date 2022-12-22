515b7a791526a723bc34c355cb21faa7349c519a
Hidayat ali <63574705+hidayatmanusiya@users.noreply.github.com>
2022-12-22 10:35:49 +0530

    BOL Update

--- kirby/kirby/doctype/bill_of_lading/bill_of_lading.js
+++ kirby/kirby/doctype/bill_of_lading/bill_of_lading.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Bill of Lading', {
-	// refresh: function(frm) {
-
-	// }
+    // refresh: function(frm) {
+    //     msgprint("Demo");
+    // },
+    booking_number: function(frm) {
+        frm.set_query("container", function() {
+            return {
+                filters: {
+                    'booking_number': cur_frm.doc.booking_number,
+                    'bill_of_lading': 0
+                }
+            }
+        });
+    }
 });
